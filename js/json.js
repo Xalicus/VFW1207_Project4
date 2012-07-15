e582e08419eb373d8683f7d15eb652c73b27e7b3
Xalicus <Xalicus@fullsail.edu>
2012-07-15 01:22:50 -0700

    Added the json.js file to my git repo.

---
+++ js/json.js
@@ -0,0 +1,22 @@
+var json = {
+	"pet1": {
+		"petsType": ["Pet Type:", "Dogs"],
+		"petName": ["Pet Name:", "Snoopy"],
+		"petEmail": ["Pet Email:", "Snoopy@petmail.com"],
+		"gender": ["Gender:", "Male"],
+		"favePet": ["Favorite Pet?:", "Yes"],
+		"birthDate": ["Birthdate:", "2005-04-15"],
+		"koolness": ["Koolness Factor:", "10"],
+		"comments": ["Comments:", "He's so cute!"]
+	},
+	"pet2": {
+		"petsType": ["Pet Type:", "Cats"],
+		"petName": ["Pet Name:", "Katty"],
+		"petEmail": ["Pet Email:", "Katty@petmail.com"],
+		"gender": ["Gender:", "Female"],
+		"favePet": ["Favorite Pet?:", "No"],
+		"birthDate": ["Birthdate:", "2012-01-01"],
+		"koolness": ["Koolness Factor:", "5"],
+		"comments": ["Comments:", "She's so cute!"]
+	}
+};
